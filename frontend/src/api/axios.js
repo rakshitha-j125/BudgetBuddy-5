38e8d11c6cf47624f2f2953c272e07de3d4ba64e
rakshitha-j125 <jvnrs125@gmail.com>
2026-09-08 23:06:00 +0530

    Fix production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,20 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+  return config;
+});
 
 export default api;
